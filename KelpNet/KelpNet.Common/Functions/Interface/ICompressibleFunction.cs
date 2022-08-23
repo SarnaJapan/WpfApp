8f6864ce2f942cb9476bd8f02fdd7b1e6ece24d9
SarnaJapan <105941103+SarnaJapan@users.noreply.github.com>
2022-08-23 14:58:29 +0900

    Delete KelpNet directory

--- KelpNet/KelpNet.Common/Functions/Interface/ICompressibleFunction.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace KelpNet.CPU
-{
-    public interface ICompressibleFunction<T> : IFunction<T> where T : unmanaged, IComparable<T>
-    {
-        ICompressibleActivation<T> Activation { get; set; }
-    }
-}
